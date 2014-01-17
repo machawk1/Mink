9bdfcf169b83c48d93189f4270514cf0ecce492b
Mat Kelly <machawk1@gmail.com>
2014-01-17 16:28:31 -0500

    Added fetching of paginated timemaps and display in the ui as a select box

--- nelson-plugin/content.js
+++ nelson-plugin/content.js
@@ -1,9 +1,10 @@
 //alert($("body").html());
 var iconUrl = chrome.extension.getURL("images/icon128.png"); 
 var iconUrlFlipped = chrome.extension.getURL("images/icon128flipped.png"); 
+var proxy = "http://mementoproxy.lanl.gov/aggr/timemap/link/1/";
 
 $("body").append("<div id=\"nelsonContainer\"></div>");
-$("#nelsonContainer").append("<div id=\"archiveOptions\">test</div>");
+$("#nelsonContainer").append("<div id=\"archiveOptions\">Fetching Mementos...</div>");
 $("#nelsonContainer").append("<img src=\""+iconUrl+"\" id=\"mLogo\" />");
 
 
@@ -18,7 +19,7 @@ function flip(){
 	
 	if(logoInFocus && w == "0px" && $("#mLogo").attr("src") == iconUrl){
 		console.log("Stopping the rotation on front of logo!");
-		showArchiveOptions();
+		
 		return;
 	}
 	
@@ -42,7 +43,8 @@ function flip(){
 
 $(document).ready(function(){
 	$("#mLogo").click(function(){
-		logoInFocus = true;
+		showArchiveOptions();
+		//logoInFocus = true;
 	});
 });
 
@@ -52,8 +54,59 @@ function showArchiveOptions(){
 		opacity: "1.0"
 	},500,null);
 }
+echoCurrentURI();
+function echoCurrentURI(uri,alreadyAcquiredTimemaps){
+	console.log("Fetching current");
+	var timemaploc = proxy + window.location;
+	if(uri){
+		timemaploc = uri;
+	}
+	$.ajax({
+		url: timemaploc,
+		type: "GET"
+	}).done(function(data,textStatus,xhr){
+		//console.log("SUCCESS!");
+		console.log(data);
+		//console.log(textStatus);
+		//console.log(xhr);
+		if(xhr.status == 200){
+			var othertimemaps = data.match(/<https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)>;rel=\"timemap\"/g);
 
+			if(othertimemaps && othertimemaps.length > 0 && othertimemaps[0] != proxy + window.location){ //the timemap contained references to other timemaps
+				var timemapURI = othertimemaps[0].substring(1,othertimemaps[0].indexOf(">"));
+				
+				
+				console.log("Going to timemap "+timemapURI);
+				//should run a filter function here instead of naive equality
+				if(!alreadyAcquiredTimemaps){
+					return echoCurrentURI(timemapURI,data);
+				}else {
+					return echoCurrentURI(timemapURI,alreadyAcquiredTimemaps+data);
+				}
+			}else if(!uri && !alreadyAcquiredTimemaps){ //only the initial timemap exists
+				alreadyAcquiredTimemaps = data;
+			}
+			
+			var matches = alreadyAcquiredTimemaps.match(/rel=\".*memento.*\"/g);
+			var dtMatches = alreadyAcquiredTimemaps.match(/datetime=\"(.*)\"/g);
 
+			var selectBox = "<select id=\"mdts\">";
+			$(dtMatches).each(function(i,v){
+				selectBox += "\t<option>"+v.substring(10,dtMatches[0].length-1)+"</option>\r\n";
+			});
+			selectBox += "</select>";
+			
+			$("#archiveOptions").html(matches.length+" mementos available." +
+				selectBox
+			);
+			logoInFocus = true;
+		}
+	}).error(function(e){
+		console.log("ERROR");
+		console.log(e);
+	});
+	//console.log("X"+window.location);
+}
 
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
