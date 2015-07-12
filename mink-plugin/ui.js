79492195a3f02e8a920d03baf950d2c7901c582c
Mat Kelly <machawk1@gmail.com>
2015-07-12 18:22:55 -0400

    Removed a Considered Harmful fix for #92.

--- mink-plugin/ui.js
+++ mink-plugin/ui.js
@@ -418,12 +418,6 @@ function showMementoCountsByYear(){
 
 			memCountList += "</ul>";
 
-			if($("#drilldownBox").length){ // Exists?
-				$("#drilldownBox").css("display","block"); //simply display and do not re-add to the DOM, for 92.
-				return;
-
-			}
-
 			$("#drilldownBox").append(memCountList);
 			$("#drilldownBox ul#years li").click(function(){
 				$("#month,#day,#time").remove();
