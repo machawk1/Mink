37c56fc2cc53591a702c408b269e3f9f0a06d4bd
Mat Kelly <me@matkelly.com>
2016-04-20 13:15:34 -0400

    Create ignore file for JS standard style checker for #219

--- mink-plugin/highchartsdata.js
+++
@@ -1,9 +0,0 @@
-function getHighchartsData(){
-
-var ret = "<div id=\"container\" style=\"min-width: 310px; height: 400px; margin: 0 auto\"></div>\r\n" +
-
-"<pre id=\"tsv\" style=\"display:none\"></p>";
-
-return ret;
-
-}
