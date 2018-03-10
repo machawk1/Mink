52ad87b66befad308724aa706e41a3f2f5481d06
Mat Kelly <me@matkelly.com>
2018-03-10 11:28:24 -0500

    Paying for my global var sins.

--- mink-plugin/js/displayMinkUI.js
+++ mink-plugin/js/displayMinkUI.js
@@ -270,9 +270,10 @@ function archiveURIArchiveDotIs (cb, openInNewTab) {
     })
 }
 
-let years = {}
-let monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-let dayNames = ['NA', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th',
+/* Vars in this namespace get "already declared" error when injected, hence var instead of let */
+var years = {}
+var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+var dayNames = ['NA', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th',
   '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th',
   '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st']
 
